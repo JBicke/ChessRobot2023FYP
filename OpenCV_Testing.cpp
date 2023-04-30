da70e556ed99705e40ed126899c4fa4f6796b4fd
James Bickerdike <bickers4472@gmail.com>
2023-04-30 22:05:22 +1000

    Finally got openCV to work on visual studio code

--- OpenCV_Testing.cpp
+++ OpenCV_Testing.cpp
@@ -1,24 +1,28 @@
 // g++ -o OpenCV_Testing OpenCV_Testing.cpp -I/usr/include/opencv4 `pkg-config --cflags --libs opencv4`
 // /OpenCV_Testing
 
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
 int main() {
-	Mat image = imread("chessboard.jpg"); // read image from file
+	Mat image = imread("C:/Users/James/Documents/Uni Units/FYP Project/ChessRobot2023FYP/chessboard.jpg"); // read image from file
 	if (image.empty()) // check if image was loaded successfully
 	{
 		cout << "Error: image not found" << endl;
 		return 1;
 	}
 	
-	
 	Mat gray;
 	cvtColor(image, gray, COLOR_BGR2GRAY);
-	
+
+	// Size boardSize(8, 8);
+	// vector<Point2f> corners;
+	// bool found = findChessboardCorners(image, boardSize, corners);
+
 	Mat edges;
 	Canny(gray, edges, 50, 150);
 	

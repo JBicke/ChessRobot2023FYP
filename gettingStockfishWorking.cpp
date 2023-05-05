1a43c378b25ea1368102677d5491857e8557bb21
JBicke <bickers4472@gmail.com>
2023-05-06 00:48:41 +1000

    Minor testing

---
+++ gettingStockfishWorking.cpp
@@ -0,0 +1,149 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstring>
+
+/**
+ * Creates two pipes, forks, and runns the given command.  One pipe is
+ * connected between the given *out and the standard input stream of the child;
+ * the other pipe is connected between the given *in and the standard output
+ * stream of the child.
+ *
+ * Returns the pid of the child on success, -1 otherwise.  On error, errno
+ * will be set accordingly.
+ */
+int bi_popen(const char* const command, FILE** const in, FILE** const out)
+{
+    const int READ_END = 0;
+    const int WRITE_END = 1;
+    const int INVALID_FD = -1;
+
+    int to_child[2] = { INVALID_FD, INVALID_FD };
+    int to_parent[2] = { INVALID_FD, INVALID_FD };
+
+    *in = NULL;
+    *out = NULL;
+
+    if (command == NULL || in == NULL || out == NULL) {
+        errno = EINVAL;
+        return 0;
+    }
+
+    if (pipe(to_child) < 0) {
+        return 0;
+    }
+
+    if (pipe(to_parent) < 0) {
+        return 0;
+    }
+
+    const pid_t pid = fork();
+    if (pid < 0) {
+        return 0;
+    }
+
+    if (pid == 0) { // Child
+        if (dup2(to_child[READ_END], STDIN_FILENO) < 0) {
+            perror("dup2");
+            exit(1);
+        }
+        close(to_child[READ_END]);
+        close(to_child[WRITE_END]);
+
+        if (dup2(to_parent[WRITE_END], STDOUT_FILENO) < 0) {
+            perror("dup2");
+            exit(1);
+        }
+        close(to_parent[READ_END]);
+        close(to_parent[WRITE_END]);
+
+        execlp(command, command, NULL);
+        perror("execlp");
+        exit(1);
+    }
+
+    // Parent
+    close(to_child[READ_END]);
+    to_child[READ_END] = INVALID_FD;
+
+    close(to_parent[WRITE_END]);
+    to_parent[WRITE_END] = INVALID_FD;
+
+    *in = fdopen(to_parent[READ_END], "r");
+    if (*in == NULL) {
+        return 0;
+    }
+    to_parent[READ_END] = INVALID_FD;
+
+    *out = fdopen(to_child[WRITE_END], "w");
+    if (*out == NULL) {
+        return 0;
+    }
+    to_child[WRITE_END] = INVALID_FD;
+
+    setvbuf(*out, NULL, _IONBF, BUFSIZ);
+
+    return pid;
+
+	// Goto label must be a statement, this is an empty statement
+    const int old_errno = errno;
+
+    if (*in != NULL) {
+        fclose(*in);
+    }
+
+    if (*out != NULL) {
+        fclose(*out);
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        if (to_child[i] != INVALID_FD) {
+            close(to_child[i]);
+        }
+        if (to_parent[i] != INVALID_FD) {
+            close(to_parent[i]);
+        }
+    }
+
+    errno = old_errno;
+    return -1;
+}
+
+int main(void)
+{
+    FILE* in = NULL;
+    FILE* out = NULL;
+    char* line = NULL;
+    size_t size = 0;
+
+    const int pid = bi_popen("stockfish", &in, &out);
+    if (pid < 0) {
+        perror("bi_popen");
+        return 1;
+    }
+
+    fprintf(out, "position startpos moves e2e4 c7c5\n");
+    fprintf(out, "go depth 10 \n");
+    
+    
+    
+    for(int i = 1; i<20;i++) {		
+		getline(&line, &size, in);
+		if (strncmp(line, "bestmove", 8) == 0) {
+            printf("%s", line);
+            break;
+        }
+	}
+    
+    // Since in this case we can tell the child to terminate, we'll do so
+    // and wait for it to terminate before we close down.
+    fprintf(out, "quit\n");
+    waitpid(pid, NULL, 0);
+
+    fclose(in);
+    fclose(out);
+
+    return 0;
+}

0fed2e831a23f66f8175ae02e54230ad874b043a
JacksonBenHul <jben0022@student.monash.edu>
2023-05-23 19:31:11 +1000

    build update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/UNI/ece4078/ChessRobot2023FYP
+# Build directory: E:/UNI/ece4078/ChessRobot2023FYP/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

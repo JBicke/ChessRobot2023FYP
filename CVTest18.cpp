921cc2de0344826c4c675e18c531bba783bcf41b
JacksonBenHul <jben0022@student.monash.edu>
2023-08-15 20:01:21 +1000

    cropping and edge detection

---
+++ CVTest18.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+//#include "opencv2/imgcodecs.hpp"
+//#include "opencv2/highgui.hpp"
+//#include "opencv2/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+std::vector<cv::Point> locateGreenSquares(const cv::Mat& inputImage)
+{
+    std::vector<cv::Point> greenSquarePositions;
+
+    // Convert the input image to the HSV color space
+    cv::Mat hsvImage;
+    cv::cvtColor(inputImage, hsvImage, cv::COLOR_BGR2HSV);
+
+    // Define lower and upper bounds for green color (in HSV)
+    cv::Scalar lowerGreen(35, 50, 50);
+    cv::Scalar upperGreen(85, 255, 255);
+
+    // Create a mask for the green color range
+    cv::Mat greenMask;
+    cv::inRange(hsvImage, lowerGreen, upperGreen, greenMask);
+
+    // Find contours in the green mask
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(greenMask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // Filter contours based on area
+    double minAreaThreshold = 100.0; // Adjust this threshold as needed
+    for (const auto& contour : contours)
+    {
+        double area = cv::contourArea(contour);
+        if (area > minAreaThreshold)
+        {
+            cv::Moments moments = cv::moments(contour, false);
+            if (moments.m00 != 0) // Avoid division by zero
+            {
+                cv::Point centroid(moments.m10 / moments.m00, moments.m01 / moments.m00);
+                greenSquarePositions.push_back(centroid);
+   
+            }
+        }
+    }
+
+    return greenSquarePositions;
+}
+
+cv::Mat rotateImageToStraight(const cv::Mat& image, cv::Point2f point1, cv::Point2f point2) {
+  // Calculate the rotation angle based on the y-coordinate difference
+    double angle = std::atan2(point2.y - point1.y, point2.x - point1.x) * 180.0 / CV_PI;
+
+    // Get the rotation matrix
+    cv::Point2f center(image.cols / 2, image.rows / 2);
+    cv::Mat rotationMatrix = cv::getRotationMatrix2D(center, angle, 1.0);
+
+    // Rotate the image
+    cv::Mat rotatedImage;
+    cv::warpAffine(image, rotatedImage, rotationMatrix, image.size());
+
+    return rotatedImage;
+}
+
+cv::Mat cannyEdgeDetection(const cv::Mat& image){
+    cv::Mat c_cannyEdges, p_cannyEdges;
+    cv::Mat cannyEdges;
+    Canny(image, cannyEdges, 100, 150, 3);
+
+    cvtColor(cannyEdges, c_cannyEdges, COLOR_GRAY2BGR);
+    p_cannyEdges = cannyEdges.clone();
+
+    vector<Vec2f> lines; // will hold the results of the detection
+    HoughLines(cannyEdges, lines, 1, CV_PI/180, 150, 0, 0 ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        //cout << "Point 1: "<< pt1.x << " " << pt1.y << " Point 2: " << pt2.x << " " << pt2.y << endl;
+        line( c_cannyEdges, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+    }
+
+    return c_cannyEdges;
+}
+
+cv::Mat probEdgeDetection(const cv::Mat& image){
+    cv::Mat probDetection;
+    vector<Vec4i> linesP; // will hold the results of the detection
+    HoughLinesP(image, linesP, 1, CV_PI/180, 20, 50, 20 ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < linesP.size(); i++ )
+    {
+        Vec4i l = linesP[i];
+        line( probDetection, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+    }
+
+    for ( size_t i = 0; i < linesP.size(); i++){
+        for ( size_t j = i + 1; j < linesP.size(); j++){
+
+            cv::Vec4i line1 = linesP[i];
+			cv::Vec4i line2 = linesP[j];
+			cv::Point pt1_1(line1[0], line1[1]);
+			cv::Point pt1_2(line1[2], line1[3]);
+			cv::Point pt2_1(line2[0], line2[1]);
+			cv::Point pt2_2(line2[2], line2[3]);
+            
+            //cout << line1 << line2 << endl;
+        }
+
+    }
+
+    return probDetection;
+}
+int main(int argc, char** argv)
+{
+    // Declare the output variables
+    Mat dst, cdst, cdstP;
+    const char* default_file ="E:/UNI/ece4078/ChessRobot2023FYP/chess_board_photos/chess_board_4.jpg";
+    const char* filename = argc >=2 ? argv[1] : default_file;
+    // Loads an image
+    Mat src = imread( samples::findFile( filename ));
+    // Check if image is loaded fine
+    if(src.empty()){
+        printf(" Error opening image\n");
+        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+        return -1;
+    }
+    
+    // Locate green square positions
+    std::vector<cv::Point> crop_points = locateGreenSquares(src);
+
+    //finding points
+    cv::Point2f  point1 = crop_points[0];
+    cv::Point2f  point2 = crop_points[1];
+
+    cout << point1 << " " << point2 << endl;
+
+    // rotate image
+    cv::Mat rotatedImage = rotateImageToStraight(src, point1, point2);
+    
+    //display rotated image
+    //cv::imshow("Rotated Image", rotatedImage);
+    
+    //locate green squares in rotated image
+    std::vector<cv::Point> greenSquarePositions = locateGreenSquares(rotatedImage);
+
+    // Display the detected green square positions
+    for (const auto& position : greenSquarePositions)
+    {
+        std::cout << "Green square position: (" << position.x << ", " << position.y << ")" << std::endl;
+    }
+
+    //finding positions of chess board corners
+    int position1_x = greenSquarePositions[2].x;
+    int position1_y = greenSquarePositions[2].y;
+
+    int position4_x = greenSquarePositions[1].x;
+    int position4_y = greenSquarePositions[1].y;
+
+    int width = position4_x - position1_x;
+    int height = position4_y - position1_y;
+
+    cout << position1_x << " " << position1_y << " " << position4_x << " " << position4_y << " " << width << " " << height << endl;
+
+    cv::Rect crop_region(position1_x, position1_y, width, height);
+    
+    Mat croppedImage = rotatedImage(crop_region);
+
+    //cv::imshow("Cropped Image", croppedImage);
+    
+    //convert to gray_scale
+    cv::Mat src_gray;
+    cvtColor(croppedImage, src_gray, COLOR_BGR2GRAY);
+    
+    //convert to binary
+    cv::Mat src_binary;
+    cv::adaptiveThreshold(src_gray, src_binary, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 2);
+    //imshow("binary", src_binary);
+
+    //find chessboard corners
+    std::vector<cv::Point2f> corners;
+    cv::Size boardSize(7,7);
+    int found = cv::findChessboardCorners(src_gray, boardSize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE );
+    //cout<< corners << endl;
+    if (found){
+        cv::cornerSubPix(src_binary, corners, cv::Size(5, 5), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1));
+
+        cv::Mat mask = cv::Mat::zeros(src.size(), CV_8U);
+        std::vector<cv::Point> polyCorners;
+        polyCorners.push_back(corners[0]);
+        polyCorners.push_back(corners[boardSize.width - 1]);
+        polyCorners.push_back(corners[boardSize.width * (boardSize.height - 1)]);
+        polyCorners.push_back(corners[boardSize.width * boardSize.height - 1]);
+        cv::fillConvexPoly(mask, polyCorners, cv::Scalar(255));
+
+        // Find contours in the mask
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        // Draw the outlines of each square
+        cv::Mat resultImage = croppedImage.clone();
+        cv::Scalar outlineColor(0, 255, 0); // Green color for contours
+        cv::drawContours(resultImage, contours, -1, outlineColor, 2);
+
+        // Display the resulting image with square outlines
+        imshow("Result", resultImage);
+    }
+    else {
+        cout << "found not found" << endl;
+    }
+    
+    //canny edge detection
+    cv::Mat cannyEdges = cannyEdgeDetection(src_gray); 
+    cv::imshow("Canny Edges Hough", cannyEdges);
+
+    //prob edge detection
+    cv::Mat probEdges = probEdgeDetection(src_gray);
+    cv::imshow("Prob Edges Hough", probEdges);
+
+
+
+
+
+
+
+    cv::waitKey(0);
+
+    return 0;
+}
---
+++ SS CV Test History/CVTest17.cpp
@@ -0,0 +1,253 @@
+#include <iostream>
+//#include "opencv2/imgcodecs.hpp"
+//#include "opencv2/highgui.hpp"
+//#include "opencv2/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Declare the output variables
+    Mat dst, cdst, cdstP;
+    const char* default_file ="E:/UNI/ece4078/ChessRobot2023FYP/chess_board_photos/chess_board_1.jpg";
+    const char* filename = argc >=2 ? argv[1] : default_file;
+    // Loads an image
+    Mat src = imread( samples::findFile( filename ));
+    // Check if image is loaded fine
+    if(src.empty()){
+        printf(" Error opening image\n");
+        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+        return -1;
+    }
+
+    cv::Mat src_gray;
+    cvtColor(src, src_gray, COLOR_BGR2GRAY); 
+    cv::Mat src_binary;
+
+    
+    cvtColor(src, src_gray, COLOR_BGR2GRAY); 
+    cv::adaptiveThreshold(src_gray, src_binary, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 2);
+
+    imshow("binary image", src_gray);
+
+    //convert image to HSV
+    cv::Mat hsvImage;
+    cv::cvtColor(src, hsvImage, cv::COLOR_BGR2HSV);
+
+    cv::Mat transMask;
+    inRange(hsvImage, cv::Scalar (0,0,143), cv::Scalar (179,61,252), transMask);
+    imshow("Binary", transMask);
+    
+    //crop chessboard image
+    cv::Scalar lowerGreen(35,50,50);
+    cv::Scalar upperGreen(85,255,255);
+
+    cv::Mat greenMask;
+
+    cv::inRange(hsvImage, lowerGreen, upperGreen, greenMask);
+
+
+
+    
+
+
+
+
+
+
+    //find chessboard corners
+    std::vector<cv::Point2f> corners;
+    cv::Size boardSize(7,7);
+    int found = cv::findChessboardCorners(src_gray, boardSize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE );
+    cout<< corners << endl;
+    if (found){
+        cv::cornerSubPix(src_binary, corners, cv::Size(5, 5), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1));
+
+        cv::Mat mask = cv::Mat::zeros(src.size(), CV_8U);
+        std::vector<cv::Point> polyCorners;
+        polyCorners.push_back(corners[0]);
+        polyCorners.push_back(corners[boardSize.width - 1]);
+        polyCorners.push_back(corners[boardSize.width * (boardSize.height - 1)]);
+        polyCorners.push_back(corners[boardSize.width * boardSize.height - 1]);
+        cv::fillConvexPoly(mask, polyCorners, cv::Scalar(255));
+
+        // Find contours in the mask
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        // Draw the outlines of each square
+        cv::Mat resultImage = src.clone();
+        cv::Scalar outlineColor(0, 255, 0); // Green color for contours
+        cv::drawContours(resultImage, contours, -1, outlineColor, 2);
+
+        // Display the resulting image with square outlines
+        imshow("Result", resultImage);
+    }
+    else {
+        cout << "found not found" << endl;
+    }
+    
+  
+    
+    
+    
+    //colour detection
+    //define colour ranges for pink and blue
+    cv::Scalar lowerPink(140,50,50);
+    cv::Scalar upperPink(180, 255, 255);
+
+    cv::Scalar lowerBlue(90,50,50);
+    cv::Scalar upperBlue(130,255,255);
+
+    //cv::Scalar lowerBlack(0,0,0);
+    //cv::Scalar upperBlack(255, 255, 30);
+    
+    //perform masking for both blue and pink intensities
+    cv::Mat pinkMask;
+    cv::Mat blueMask;
+    //cv::Mat blackMask;
+
+    cv::inRange(hsvImage, lowerPink, upperPink, pinkMask);
+    cv::inRange(hsvImage, lowerBlue, upperBlue, blueMask);
+    //cv::inRange(hsvImage, lowerBlack, upperBlack, blackMask);
+
+    cv::Mat resultMask = pinkMask | blueMask;
+
+    // Perform morphological operations on the combined mask
+    int morphSize = 3;  // Adjust the size as per your requirement
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(morphSize, morphSize));
+    cv::morphologyEx(resultMask, resultMask, cv::MORPH_OPEN, kernel);
+
+    // Find contours in the combined mask
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(resultMask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // Draw bounding rectangles around the detected contours
+    cv::Mat PinkBlueImage = src.clone();
+    cv::Scalar contourColour;
+    cv::Scalar pinkColour(255, 0, 255);  // Green color for contour bounding boxes
+    cv::Scalar blueColour(255,0,0);
+    for (size_t i = 0; i < contours.size(); ++i) {
+        cv::Rect boundingRect = cv::boundingRect(contours[i]);
+        if (cv::countNonZero(pinkMask(boundingRect)) > 0)
+            contourColour = pinkColour;
+        else
+            contourColour = blueColour;
+        cv::rectangle(PinkBlueImage, boundingRect, contourColour, 2);
+    }
+    
+
+    cv::imshow( "HSV Image", PinkBlueImage);
+
+    cv::Mat adaptiveThreshold;
+    cv::adaptiveThreshold(src_gray, adaptiveThreshold, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
+
+    cv::Mat c_cannyEdges, p_cannyEdges;
+    cv::Mat cannyEdges;
+    Canny(src_gray, cannyEdges, 100, 150, 3);
+
+    cvtColor(cannyEdges, c_cannyEdges, COLOR_GRAY2BGR);
+    p_cannyEdges = cannyEdges.clone();
+
+    vector<Vec2f> lines; // will hold the results of the detection
+    HoughLines(cannyEdges, lines, 1, CV_PI/180, 150, 0, 0 ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        cout << "Point 1: "<< pt1.x << " " << pt1.y << " Point 2: " << pt2.x << " " << pt2.y << endl;
+        line( c_cannyEdges, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+    }
+
+    cv::imshow("Canny EDges Hough", c_cannyEdges);
+
+
+    cv::Mat blackSquareEdges = adaptiveThreshold & ~resultMask;
+
+    std::vector<std::vector<cv::Point>> blackContours;
+    cv::findContours(blackSquareEdges, blackContours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    cv::Mat blackSquaresImage = src.clone();
+    cv::drawContours(blackSquaresImage, blackContours, -1, cv::Scalar(0, 0, 255), 2);
+    cv::imshow("Black Squares Image", blackSquaresImage);
+
+    /*
+    std::vector<cv::Point> pinkPiecePositions, bluePiecePositions;
+    for (const auto& contour : blackContours)
+    {   
+        
+        double area = cv::contourArea(contour);
+        if (area > 0 && area < 500) // Adjust the area range based on the piece size
+        {
+            
+            // Determine if the piece is pink or blue by checking color mask intersections
+            cv::Mat mask = cv::Mat::zeros(src.size(), CV_8U);
+            if (contour.size() > 0)
+            {
+                std::vector<std::vector<cv::Point>> temp_contour;
+                temp_contour.push_back(contour);
+                cv::drawContours(mask, temp_contour, 0, cv::Scalar(255), -1);
+                
+                int blueIntersection = cv::countNonZero(resultMask & mask);
+                int pinkIntersection = cv::countNonZero(pinkMask & mask);
+
+               if (blueIntersection > pinkIntersection)
+                {
+                    // Calculate the centroid of the contour for blue piece
+                    cv::Moments moments = cv::moments(contour, false); // Use binaryImage=false to get accurate moments
+                    cv::Point centroid(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+                    // Ensure the centroid is within the valid image range
+                    if (centroid.x >= 0 && centroid.x < src.cols && centroid.y >= 0 && centroid.y < src.rows)
+                        bluePiecePositions.push_back(centroid);
+                    else
+                        std::cout << "Invalid centroid for blue piece: " << centroid << std::endl;
+                }
+                else
+                {
+                    // Calculate the centroid of the contour for pink piece
+                    cv::Moments moments = cv::moments(contour, false); // Use binaryImage=false to get accurate moments
+                    cv::Point centroid(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+                    // Ensure the centroid is within the valid image range
+                    if (centroid.x >= 0 && centroid.x < src.cols && centroid.y >= 0 && centroid.y < src.rows)
+                        pinkPiecePositions.push_back(centroid);
+                    else
+                        std::cout << "Invalid centroid for pink piece: " << centroid << std::endl;
+                }
+            }
+        }
+    }
+    
+    // Create 8x8 matrix to represent chessboard positions
+    const int rows = 8;
+    const int cols = 8;
+    cv::Mat chessboard(rows, cols, CV_8S, cv::Scalar(0)); // Initialize matrix with 0 (empty)
+    cout << pinkPiecePositions << endl;
+    cout << bluePiecePositions << endl;
+    
+    // Assign -1 for pink pieces and 1 for blue pieces in the matrix
+    for (const auto& pinkPiecePos : pinkPiecePositions)
+        chessboard.at<char>(pinkPiecePos) = -1;
+    for (const auto& bluePiecePos : bluePiecePositions)
+        chessboard.at<char>(bluePiecePos) = 1;
+
+    // Display the chessboard positions matrix
+    std::cout << chessboard << std::endl;
+    */
+
+
+    cv::waitKey(0);
+}

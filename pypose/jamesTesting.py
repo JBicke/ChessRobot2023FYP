5622c4e906cb5424cd8bc2a3d2e8ba40a6d379cf
JBicke <bickers4472@gmail.com>
2023-08-17 02:21:13 +1000

    More Testing

--- pypose/jamesTesting.py
+++ pypose/jamesTesting.py
@@ -50,101 +50,105 @@ class Driver:
         self.ser.flushInput()
         length = 2 + len(params)
         checksum = 255 - ((index + length + ins + sum(params))%256)
-        self.ser.write(chr(0xFF)+chr(0xFF)+chr(index)+chr(length)+chr(ins))
+        self.ser.write(bytes([0xFF,0xFF,index,length,ins]))
+        print(bytes([0xFF,0xFF,index,length,ins]))
         for val in params:
-            self.ser.write(chr(val))
-        self.ser.write(chr(checksum))
-        return self.getPacket(0)
-
-    def setReg(self, index, regstart, values):
-        """ Set the value of registers. Should be called as such:
-        ax12.setReg(1,1,(0x01,0x05)) """
-        self.execute(index, AX_WRITE_DATA, [regstart] + values)
-        return self.error
-
-    def getPacket(self, mode, id=-1, leng=-1, error=-1, params = None):
-        """ Read a return packet, iterative attempt """
-        # need a positive byte
-        d = self.ser.read()
-        if d == '':
-            self.logger.debug("Fail Read")
-            return None
-
-        # now process our byte
-        if mode == 0:           # get our first 0xFF
-            if ord(d) == 0xff:
-                self.logger.debug("Oxff found")
-                return self.getPacket(1)
-            else:
-                self.logger.debug("Oxff NOT found, restart: " + str(ord(d)))
-                return self.getPacket(0)
-        elif mode == 1:         # get our second 0xFF
-            if ord(d) == 0xff:
-                self.logger.debug("Oxff found")
-                return self.getPacket(2)
-            else:
-                self.logger.debug("Oxff NOT found, restart: " + str(ord(d)))
-                return self.getPacket(0)
-        elif mode == 2:         # get id
-            if d != 0xff:
-                self.logger.debug("ID found: " + str(ord(d)))
-                return self.getPacket(3, ord(d))
-            else:
-                self.logger.debug("0xff is not ID, restart")
-                return self.getPacket(0)
-        elif mode == 3:         # get length
-            self.logger.debug("Length found: " + str(ord(d)))
-            return self.getPacket(4, id, ord(d))
-        elif mode == 4:         # read error
-            self.logger.debug("Error level found: " + str(ord(d)))
-            self.error = ord(d)
-            if leng == 2:
-                return self.getPacket(6, id, leng, ord(d), list())
-            else:
-                return self.getPacket(5, id, leng, ord(d), list())
-        elif mode == 5:         # read params
-            self.logger.debug("Parameter found: " + str(ord(d)))
-            params.append(ord(d))
-            if len(params) + 2 == leng:
-                return self.getPacket(6, id, leng, error, params)
-            else:
-                return self.getPacket(5, id, leng, error, params)
-        elif mode == 6:         # read checksum
-            self.logger.debug("Checksum found: " + str(ord(d)))
-            checksum = id + leng + error + sum(params) + ord(d)
-            self.logger.debug("Checksum computed: " + str(checksum))
-            if checksum % 256 != 255:
-                self.logger.debug("Checksum ERROR")
-                return None
-            return params
-        # fail
+            self.ser.write(bytes([val]))
+        self.ser.write(bytes([checksum]))
+        print("OK")
+        # return self.getPacket(0)
         return None
+
+
+    # def setReg(self, index, regstart, values):
+        # """ Set the value of registers. Should be called as such:
+        # ax12.setReg(1,1,(0x01,0x05)) """
+        # self.execute(index, AX_WRITE_DATA, [regstart] + values)
+        # return self.error
+
+    # def getPacket(self, mode, id=-1, leng=-1, error=-1, params = None):
+        # """ Read a return packet, iterative attempt """
+        # # need a positive byte
+        # d = self.ser.read()
+        # if d == '':
+            # self.logger.debug("Fail Read")
+            # return None
+
+        # # now process our byte
+        # if mode == 0:           # get our first 0xFF
+            # if ord(d) == 0xff:
+                # self.logger.debug("Oxff found")
+                # return self.getPacket(1)
+            # else:
+                # self.logger.debug("Oxff NOT found, restart: " + str(ord(d)))
+                # return self.getPacket(0)
+        # elif mode == 1:         # get our second 0xFF
+            # if ord(d) == 0xff:
+                # self.logger.debug("Oxff found")
+                # return self.getPacket(2)
+            # else:
+                # self.logger.debug("Oxff NOT found, restart: " + str(ord(d)))
+                # return self.getPacket(0)
+        # elif mode == 2:         # get id
+            # if d != 0xff:
+                # self.logger.debug("ID found: " + str(ord(d)))
+                # return self.getPacket(3, ord(d))
+            # else:
+                # self.logger.debug("0xff is not ID, restart")
+                # return self.getPacket(0)
+        # elif mode == 3:         # get length
+            # self.logger.debug("Length found: " + str(ord(d)))
+            # return self.getPacket(4, id, ord(d))
+        # elif mode == 4:         # read error
+            # self.logger.debug("Error level found: " + str(ord(d)))
+            # self.error = ord(d)
+            # if leng == 2:
+                # return self.getPacket(6, id, leng, ord(d), list())
+            # else:
+                # return self.getPacket(5, id, leng, ord(d), list())
+        # elif mode == 5:         # read params
+            # self.logger.debug("Parameter found: " + str(ord(d)))
+            # params.append(ord(d))
+            # if len(params) + 2 == leng:
+                # return self.getPacket(6, id, leng, error, params)
+            # else:
+                # return self.getPacket(5, id, leng, error, params)
+        # elif mode == 6:         # read checksum
+            # self.logger.debug("Checksum found: " + str(ord(d)))
+            # checksum = id + leng + error + sum(params) + ord(d)
+            # self.logger.debug("Checksum computed: " + str(checksum))
+            # if checksum % 256 != 255:
+                # self.logger.debug("Checksum ERROR")
+                # return None
+            # return params
+        # # fail
+        # return None
 
     def getReg(self, index, regstart, rlength):
         """ Get the value of registers, should be called as such:
         ax12.getReg(1,1,1) """
         vals = self.execute(index, AX_READ_DATA, [regstart, rlength])
         if vals == None:
-            self.logger.debug("Read Failed: Servo ID = " + str(index))
+            print("Read Failed: Servo ID = " + str(index))
             return -1
         if rlength == 1:
             return vals[0]
         return vals
 
-    def syncWrite(self, regstart, vals):
-        """ Set the value of registers. Should be called as such:
-        ax12.syncWrite(reg, ((id1, val1, val2), (id2, val1, val2))) """
-        self.ser.flushInput()
-        length = 4
-        valsum = 0
-        for i in vals:
-            length = length + len(i)
-            valsum = valsum + sum(i)
-        checksum = 255 - ((254 + length + AX_SYNC_WRITE + regstart + len(vals[0]) - 1 + valsum)%256)
-        # packet: FF FF ID LENGTH INS(0x03) PARAM .. CHECKSUM
-        self.ser.write(chr(0xFF)+chr(0xFF)+chr(0xFE)+chr(length)+chr(AX_SYNC_WRITE)+chr(regstart)+chr(len(vals[0])-1))
-        for servo in vals:
-            for value in servo:
-                self.ser.write(chr(value))
-        self.ser.write(chr(checksum))
-        # no return info...
+    # def syncWrite(self, regstart, vals):
+        # """ Set the value of registers. Should be called as such:
+        # ax12.syncWrite(reg, ((id1, val1, val2), (id2, val1, val2))) """
+        # self.ser.flushInput()
+        # length = 4
+        # valsum = 0
+        # for i in vals:
+            # length = length + len(i)
+            # valsum = valsum + sum(i)
+        # checksum = 255 - ((254 + length + AX_SYNC_WRITE + regstart + len(vals[0]) - 1 + valsum)%256)
+        # # packet: FF FF ID LENGTH INS(0x03) PARAM .. CHECKSUM
+        # self.ser.write(chr(0xFF)+chr(0xFF)+chr(0xFE)+chr(length)+chr(AX_SYNC_WRITE)+chr(regstart)+chr(len(vals[0])-1))
+        # for servo in vals:
+            # for value in servo:
+                # self.ser.write(chr(value))
+        # self.ser.write(chr(checksum))
+        # # no return info...

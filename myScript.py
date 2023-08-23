c2fdf2e88645d6bbe7896044c6cf7f89f739aac1
JBicke <bickers4472@gmail.com>
2023-08-23 14:57:17 +1000

    refresh

--- myScript.py
+++ myScript.py
@@ -2,10 +2,10 @@ from pypose.driver import Driver
 
 driver = Driver(port='/dev/ttyUSB0')
 
-from pypose.ax12 import P_MOVING, P_GOAL_SPEED_L
+from pypose.ax12 import P_ID, P_MOVING, P_GOAL_SPEED_L
 
-# driver.setReg(1,1,(0x01,0x05))
+driver.setReg(1,P_ID,(0x01))
 
-s_moving = driver.getReg(1,P_MOVING, 1)
-speed = 888
-driver.setReg(0,P_GOAL_SPEED_L, [speed % 256, speed >>8])
+# detect_ID = driver.getReg(0,2, 1)
+#speed = 888
+#driver.setReg(0,P_GOAL_SPEED_L, [speed % 256, speed >>8])

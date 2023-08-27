77d8de47dc82e849e896c3d5c27a8b3c3e856eff
JBicke <bickers4472@gmail.com>
2023-08-27 22:19:51 +1000

    Can now pick up a chess piece

--- capture_image.cpp
+++ capture_image.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main() {
-	system("fswebcam -r 1280x960 image1.jpg"); //Captures image and saves to file
+	system("fswebcam -r 1280x960 TestA.jpg"); //Captures image and saves to file
 	cout << "Image Captured" << endl;
 	return 0;
 }
--- motor controls/linear_acc.py
+++ motor controls/linear_acc.py
@@ -3,10 +3,56 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.OUT)
+time.sleep(2)
+GPIO.setup(4, GPIO.OUT)
 
-GPIO.output(13, GPIO.HIGH)
-time.sleep(10)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+# GPIO.output(4, GPIO.HIGH)
+# time.sleep(2)
+# GPIO.output(21, GPIO.HIGH)
+# time.sleep(2)
+# GPIO.output(20, GPIO.HIGH)
+# time.sleep(2)
+# GPIO.output(26, GPIO.HIGH)
+# time.sleep(2)
+
+
+# GPIO.output(4, GPIO.LOW)
+# time.sleep(2)
+# GPIO.output(21, GPIO.LOW)
+# time.sleep(2)
+# GPIO.output(20, GPIO.LOW)
+# time.sleep(2)
+# GPIO.output(26, GPIO.LOW)
+# time.sleep(2)
+
+GPIO.output(21, GPIO.HIGH)
+GPIO.output(26, GPIO.LOW)
+time.sleep(2)
+
+GPIO.output(20, GPIO.HIGH)
+time.sleep(2)
+
+
+GPIO.output(21, GPIO.LOW)
+GPIO.output(26, GPIO.HIGH)
+time.sleep(2)
+
+
+
+GPIO.output(21, GPIO.HIGH)
+GPIO.output(26, GPIO.LOW)
+time.sleep(2)
+
+GPIO.output(20, GPIO.LOW)
+time.sleep(2)
+
+GPIO.output(21, GPIO.LOW)
+GPIO.output(26, GPIO.HIGH)
+time.sleep(2)
 
 GPIO.cleanup()
 

1bdb84c09a154013be65a013cd7175914f465e71
JBicke <bickers4472@gmail.com>
2023-09-27 16:37:37 +1000

    revert 1

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import mode1
 mode = 0
 mode = input("Type 1 for input game, 2 for automated game, or 3 for interactive game:")
 if mode == "1":
-    process = subprocess.Popen(['python', 'mode1.py'])
-    process.wait()
+    print("This hasn't been integrated yet")
+    #process = subprocess.Popen(['python', 'mode1.py'])
+    #process.wait()
 elif mode == "2":
     print("This hasn't been integrated yet")
     #process = subprocess.Popen(['python', 'mode2.py'])

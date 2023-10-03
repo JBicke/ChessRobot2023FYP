c2bd8545f80e5e842a50bc2d799b5080f9857047
JBicke <bickers4472@gmail.com>
2023-10-03 15:09:14 +1100

    mini e

--- capture_image.cpp
+++ capture_image.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
 	string filename = argv[1];
-	string command = "fswebcam -r 1280x960 " + filename + ".jpg"
+	string command = "fswebcam -r 1280x960 " + filename + ".jpg";
 
 	system(command.c_str()); //Captures image and saves to file
 	// cout << "Image Captured" << endl;

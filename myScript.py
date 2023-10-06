50884fa647464f3200528f7f64f067e60da3a03f
JBicke <bickers4472@gmail.com>
2023-10-06 18:26:44 +1100

    MoreTest

--- myScript.py
+++ myScript.py
@@ -1,11 +1,5 @@
-from pypose.driver import Driver
+from pypose import runRobot
+import subprocess
+import operations
 
-driver = Driver(port='/dev/ttyUSB0')
-
-from pypose.ax12 import P_ID, P_MOVING, P_GOAL_SPEED_L
-
-driver.setReg(1,P_ID,(0x01))
-
-# detect_ID = driver.getReg(0,2, 1)
-#speed = 888
-#driver.setReg(0,P_GOAL_SPEED_L, [speed % 256, speed >>8])
+runRobot.calibrationMode(500)

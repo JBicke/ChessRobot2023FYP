b917cae980cec01a01302623bd94cd2ac56c9db1
JBicke <bickers4472@gmail.com>
2023-10-07 12:29:03 +1100

    more work

--- pypose/runRobot.py
+++ pypose/runRobot.py
@@ -432,13 +432,13 @@ def move_object(direction):
     global object_position
     
 
-def calibrationModeRight(direction,object_position):
+def calibrationModeS2(direction,object_position):
     object_position += direction * 1
     #print(f"Object position: {object_position}")
-	driver.setReg(1,P_GOAL_POSITION_L, [object_position%256,object_position>>8])
+	driver.setReg(2,P_GOAL_POSITION_L, [object_position%256,object_position>>8])
     return object_position
 
-def calibrationModeLeft(direction,object_position):
+def calibrationModeS1(direction,object_position):
     object_position += direction * 1
 	# print(f"Object position: {object_position}")
 	driver.setReg(1,P_GOAL_POSITION_L, [object_position%256,object_position>>8])

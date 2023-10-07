bf58e1b3024a0cd62c53316babaa6b60b0bd6081
JBicke <bickers4472@gmail.com>
2023-10-07 14:28:07 +1100

    again

--- takePhoto.py
+++ takePhoto.py
@@ -12,4 +12,10 @@ cpp_process.stdout.close()
 cpp_process.wait()
 
 cpp_process = subprocess.Popen(["./chessRobot7"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+cpp_process.stdin.write(photoName)
+cpp_process.stdin.flush()
+
+cpp_process.stdin.close()
+cpp_process.stdout.close()
+cpp_process.wait()
 #response_from_cpp = cpp_process.stdout.readline()
